6fdc194744ffb8384462fd036bf9b3ee57f2d4b8
FursykIvan <ivan.fursyk@gmail.com>
2024-12-08 23:15:17 +0200

    Create Pizzaiolo model with experience field
    - Create Pizzaiolo model extending AbstractUser
    - Add 'year_of_experience' field to store years of experience for Pizzaiolo
    - Set ordering by 'username' and define verbose names for singular and plural forms

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Pizzaiolo(AbstractUser):
+    year_of_experience = models.PositiveSmallIntegerField(default=0, blank=True, null=True)
+
+    class Meta:
+        ordering = ("username",)
+        verbose_name = "Pizzaiolo"
+        verbose_name_plural = "Pizzaioli"
+
+
+    def __str__(self):
+        return self.username
--- pizzeria_django_project/settings.py
+++ pizzeria_django_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "accounts",
+    "kitchen",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,20 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+LOGIN_REDIRECT_URL = "/"
+
+# email
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_PORT = 587
+
+# ttl activation token
+PASSWORD_RESET_TIMEOUT = 14400
+
+AUTH_USER_MODEL = "accounts.Pizzaiolo"
